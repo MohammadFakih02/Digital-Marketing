917a5748e2c215e62c38757800bfe7b9a89c17aa
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-11-04 13:57:09 +0200

    added animations to navbar, hero section, and about us title

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,33 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuContent = document.getElementById("mobile-menu-content");
-mobileMenu?.addEventListener("click",function(){
+mobileMenu?.addEventListener("click", function () {
     mobileMenuContent?.classList.toggle("hidden")
 })
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+   });
+gsap.from(".nav",{opacity:0,y:-100,duration:1.5,ease:"bounce.out"});
+
+gsap.to(".hero-text",{
+    duration: 2,
+    text: "Grow Your Business With Customized Digital Marketing"
+});
+
+gsap.from(".about-title",{
+    duration:1,
+    x:"-100vw",
+    scrollTrigger:{
+        trigger:".about-title",
+        end: '+=300', 
+        scrub:0.5
+    }
+});
+gsap.to(".about-subtitle",{
+    duration: 2,
+    scrollTrigger:{
+        trigger:".about-subtitle",
+        end: '+=250', 
+        scrub:0.5 
+    },
+    text: "Social Reach is a dynamic, versatile and full-service digital marketing agency that doesn’t rely on smoke and mirrors to attract new clients. Instead, Social Reach trusts its own search engine optimization (SEO) and marketing skills to drive new customers to our website."
+});
