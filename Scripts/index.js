d901a8f33fe980e425bf1c3f9af76195a4a79d2a
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-11-04 14:11:33 +0200

    added styling for about-us section

--- Scripts/index.js
+++ Scripts/index.js
@@ -31,3 +31,49 @@ gsap.to(".about-subtitle",{
     },
     text: "Social Reach is a dynamic, versatile and full-service digital marketing agency that doesn’t rely on smoke and mirrors to attract new clients. Instead, Social Reach trusts its own search engine optimization (SEO) and marketing skills to drive new customers to our website."
 });
+
+gsap.from(".about-text1",{
+    duration: 1,
+    x:"-100vw",
+    scrollTrigger:{
+        trigger:".about-text1",
+        end: '+=300', 
+        scrub:0.5
+    }
+});
+
+gsap.from(".about-image1",{
+    duration:1,
+    y:"100vh",
+    opacity:0,
+    scrollTrigger:{
+        trigger:".about-text1",
+        end: '+=300', 
+        scrub:0.5
+    }
+});
+gsap.from(".about-image2",{
+    duration:1,
+    y:"100vh",
+    opacity:0,
+    scrollTrigger:{
+        trigger:".about-text2",
+        end: '+=300', 
+        scrub:0.5
+    }
+});
+gsap.from(".about-text2",{
+    duration: 1,
+    x:"100vw",
+    scrollTrigger:{
+        trigger:".about-text2",
+        end: '+=300', 
+        scrub:0.5
+    }
+}); 
+
+gsap.to(".hero-text",{
+    duration: 2,
+    text: "Grow Your Business With Customized Digital Marketing"
+}); 
+
